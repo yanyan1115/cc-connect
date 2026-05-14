d62347d66c8ea18e32d3cf5ec7cd58d1ca6c7fcd
yanyan1115 <clearcqwz@gmail.com>
2026-05-14 09:35:01 -0400

    Revert "feat: add session project groups"
    This reverts commit 00b563871552c0979772e4fc23da4ff82eec87c4.

--- core/management.go
+++ core/management.go
@@ -1052,7 +1052,6 @@ func (m *ManagementServer) handleProjectSessions(w http.ResponseWriter, r *http.
 			info := map[string]any{
 				"id":            s.ID,
 				"name":          displayName,
-				"project":       s.Project,
 				"session_key":   idToKey[s.ID],
 				"agent_type":    s.AgentType,
 				"active":        activeIDs[s.ID],
@@ -1155,7 +1154,6 @@ func (m *ManagementServer) handleProjectSessionDetail(w http.ResponseWriter, r *
 		data := map[string]any{
 			"id":               s.ID,
 			"name":             displayName,
-			"project":          s.Project,
 			"session_key":      sessionKey,
 			"agent_session_id": s.AgentSessionID,
 			"agent_type":       s.AgentType,
